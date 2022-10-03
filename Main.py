bb6dba6ae56f2c08050342ffc724a1d6129a059e
agnespaulurk21cs1020 <109952516+agnespaulurk21cs1020@users.noreply.github.com>
2022-10-03 16:17:18 +0530

    fifth commit

--- Main.py
+++ Main.py
@@ -1,11 +1,10 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  x=[]
-  y=[]
-  x = nums1[0:m+1]
-  y = nums2[0:n+1]
-  nums1 = x.extend(y)
+ 
+  x = nums1[0:m]
+  y = nums2[0:n]
+  nums1=x+y
   nums1 = merge_sort(nums1)
   
   

5122ee4ba54044b2bc80d8a9d930b76e066333e3
agnespaulurk21cs1020 <109952516+agnespaulurk21cs1020@users.noreply.github.com>
2022-10-03 16:18:55 +0530

    sixth commit

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   y = nums2[0:n]
   nums1=x+y
   nums1 = merge_sort(nums1)
-  
+  return nums1
   
 def merge_sort(myList) -> None:
   if len(myList) > 1:
@@ -59,5 +59,5 @@ for item in input().split(', '):
   nums2.append(int(item))
 m = int(input())
 n = int(input())
-merge(nums1, m, nums2, n)
-print(nums1)
+
+print(merge(nums1, m, nums2, n))
